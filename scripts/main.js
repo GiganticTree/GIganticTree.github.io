b5bb21b3fc8e8d86f70ab158668f669ec356dee4
Gigantic Tree <seqs@dartmouth.edu>
2018-11-14 22:52:40 -0500

    Added some javascript easter egg clue, overflow scroll horizontal on menu bar

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@
 //it will cause a pop-up box with the words "hey! quite click..."
 var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+/*myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/shadow-ridge-logo') {
         myImage.setAttribute ('src', 'images/firefox2.jpg');
@@ -15,6 +15,35 @@ myImage.onclick = function() {
     } else {
         myImage.setAttribute ('src', 'images/shadow-ridge-logo');
     }
+} */
+
+
+var myClue1 = document.querySelector("#clue1").onclick = function() {
+    var x = document.getElementById("tidbit1"); {
+    
+        x.style.display = "block";
+    }
+    var n = document.getElementById("clue1");
+        n.style.display = "none";
+   
+}
+
+var myClue1hover = document.querySelector("#clue1").onmouseenter = function() {
+    var z = document.getElementById("clue1");
+        z.style.left = 80 + "px";
+}
+
+var myClue1hover = document.querySelector("#tidbit1").onmouseover = function() {
+    var t = document.getElementById("tidbit1");
+        t.style.left = 140 + "px";
+}
+
+var myClue2 = document.querySelector("#tidbit1").onclick = function() {
+    var y = document.getElementById("tidbit2");
+    
+        y.style.display = "block";
+        
+   
 }
 
 var myButton = document.querySelector('button');
@@ -34,6 +63,3 @@ var myHeading = document.querySelector('h1');
 //    myHeading.textContent = 'Shadow Ridge is cool, ' + storedName;
 //}
 
-myButton.onclick = function() {
-    setUserName();
-}
